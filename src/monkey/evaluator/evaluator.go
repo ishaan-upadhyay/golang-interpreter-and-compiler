effb7f7388a22d09cd9500e680f46a2c04112ce0
Ishaan <ishaanupadhyay1004@gmail.com>
2024-08-24 21:56:42 -0400

    Begin evaluator - support integer and boolean literals

---
+++ src/monkey/evaluator/evaluator.go
@@ -0,0 +1,50 @@
+package evaluator
+
+import (
+	"monkey/ast"
+	"monkey/object"
+)
+
+// Since there are only two boolean literals, we instantiate an instance of each
+// up-front and re-use them throughout.
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return &object.Boolean{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
